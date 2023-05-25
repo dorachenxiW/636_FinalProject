f1622d8a2c819dc52e142f11b037607f582a0d6a
dorachenxiW <Dora.Wang@lincolnuni.ac.nz>
2023-05-25 15:35:28 +1200

    sql change

--- nzoly_structure_data_pa.sql
+++ nzoly_structure_data_pa.sql
@@ -58,20 +58,23 @@ ON DELETE CASCADE
 );
 
 
-INSERT INTO Events VALUES (1,"Slopestyle","Snowboarding",101),
+INSERT INTO events VALUES 
+(1,"Slopestyle","Snowboarding",101),
 (3,"Big Air","Snowboarding",101),
 (5,"Men's Halfpipe","Freestyle Skiing",103),
 (6,"Men's 20 km individual biathlon","Biathlon",123),
 (7,"Women's slopestyle","Freestyle Skiing",103),
 (11,"Men's Halfpipe","Snowboarding",101);
 
-INSERT INTO teams VALUES (101,"Snowboard"),
+INSERT INTO teams VALUES 
+(101,"Snowboard"),
 (103,"Freestyle Skiing"),
 (123,"Biathlon"),
 (102,"Alpine Skiing"),
 (125,"Speed Skating");
 
-INSERT INTO members VALUES (5629,103,"Ben","Barclay","Auckland","2002-02-04"),
+INSERT INTO members VALUES 
+(5629,103,"Ben","Barclay","Auckland","2002-02-04"),
 (5630,103,"Anja","Barugh","Morrinsville","1999-05-21"),
 (5631,103,"Finn","Bilous","Wanaka","1999-09-22"),
 (5632,103,"Margaux","Hackett","Wanaka","1999-06-02"),
@@ -90,6 +93,7 @@ INSERT INTO event_stage VALUES
 (289,"Qualification",11,"Genting Snow Park","2022-02-17",TRUE, 70.00),
 (290,"Final",11,"Genting Snow Park","2022-02-19",FALSE, NULL);
 
-INSERT INTO event_stage_results VALUES (467,356,5634,176.5,NULL),
+INSERT INTO event_stage_results VALUES 
+(467,356,5634,176.5,NULL),
 (538,357,5634,177,2),(567,377,5634,86.75,NULL),(577,379,5634,92.88,1),
 (265,281,5632,54.93,NULL),(222,289,5633,90.50,NULL),(223,290,5633,93,1);
